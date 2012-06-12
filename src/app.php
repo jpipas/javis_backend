779281ad91987dce2cda6b75dd35bb665b4e983a
Jeff Pipas <jpipas@gmail.com>
2012-06-12 00:35:22 -0400

    symfony config

--- src/app.php
+++ src/app.php
@@ -3,13 +3,60 @@
 /**
 * Bootstrap
 **/
-//$client = require __DIR__.'/client.php';
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+
+
+$client = require __DIR__.'/client.php';
 //$territory = require __DIR__.'/territory.php';
 
-$app = require __DIR__.'/bootstrap.php';
 
-$app['debug'] = true;
+$app = new Silex\Application();
 
-$app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+require __DIR__ . '/config.php';
+
+//$app->register(new HttpCacheServiceProvider());
+
+$app->register(new SessionServiceProvider());
+//$app->register(new ValidatorServiceProvider());
+//$app->register(new FormServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile'       => __DIR__.'/../log/app.log',
+    'monolog.name'          => 'app',
+    'monolog.level'         => 300 // = Logger::WARNING
+));
+
+$app->register(new DoctrineServiceProvider(), array(
+    'db.options'    => array(
+        'driver'    => $app['db.config.driver'],
+        'dbname'    => $app['db.config.dbname'],
+        'host'      => $app['db.config.host'],
+        'user'      => $app['db.config.user'],
+        'password'  => $app['db.config.password'],
+        'port'      => $app['db.config.port'], 
+    )
+));
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/../views',
+));
+
+// Temporary hack. Silex should start session on demand.
+$app->before(function() use ($app) {
+    $app['session']->start();
 });
+
+$app->mount('/client',$client);
--- src/client.php
+++ src/client.php
@@ -1,1 +1,12 @@
 <?php
+use Silex\ControllerCollection;
+$client = new ControllerCollection();
+
+
+$client->get('/{name}', function($name) use($client) { 
+    return 'Client Hello '.$client->escape($name); 
+});
+
+
+
+return $client;
